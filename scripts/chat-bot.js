0480d2cdcdcbda4eb496e31740591afbe0f2b70d
Sathesh Rgs <satheshrgs@gmail.com>
2018-02-16 19:10:43 +0530

    Update chat-bot.js

--- scripts/chat-bot.js
+++ scripts/chat-bot.js
@@ -10,7 +10,7 @@
 		var i="</div>";
 		var j="<iframe src='http://infocruise.herokuapp.com/web' scrolling='no' frameborder='0' width='365' height='450' id='ifr'></iframe>";
 	var k="</div>";
-	var l="<div id='messenger-img' onclick='maximizechat()'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Facebook_Messenger_logo.svg/2000px-Facebook_Messenger_logo.svg.png'></div>";
+	var l="<div id='messenger-img' onclick='maximizechat()'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Facebook_Messenger_logo.svg/2000px-Facebook_Messenger_logo.svg.png' id='minbtnimg'></div>";
 	$("body").append(a);
 	$("#agent").append(b);
 	$("#botheader").append(c);
